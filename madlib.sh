9dfa84877b7537533c23e108233ba1b4bd470b4c
Amelia Vasiliu <ega9cw@virginia.edu>
2024-09-22 18:24:06 +0000

    this is my madlib project

---
+++ madlib.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+clear
+echo "Let's build a mad-lib!"
+
+read -p "1. Name an unusual sport: " NOUN1
+
+read -p "2. Name an interesting animal: " NOUN2
+
+read -p "3. Name a verb: " VERB1
+
+read -p "4. Name an adverb: " ADVERB1
+
+read -p "5. Name an adjective: " ADJECTIVE1
+
+read -p "6. Name a verb: " VERB2
+
+read -p "7. Name an adverb: " ADVERB2
+
+read -p "8. Name an adjective: " ADJECTIVE2
+
+echo "Once upon a time, there was a $NOUN2 that adored $NOUN1. One day, $NOUN2 accidentally $ADVERB1 $VERB1 to the meadow. It was a/an $ADJECTIVE1 sight. And then, $NOUN2 almost forgot about $NOUN1. They began to feel $ADJECTIVE2. So, $NOUN2 $ADVERB2 $VERB2 all the way back. The end. "
+
